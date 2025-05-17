4d533b384edc947df7ba8fcad50b92b85b68d558
swyftos <98902001+swyftos@users.noreply.github.com>
2025-05-18 00:12:21 +0200

    Update test1motor.ino

--- code_motor/test1motor.ino
+++ code_motor/test1motor.ino
@@ -4,27 +4,19 @@ const int IN1 = 3;  // Direction 1
 const int IN2 = 4;  // Direction 2
 
 void setup() {
-  // Configuration des broches en sortie
+  
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+
+  // Sens de rotation : 
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // Vitesse fixe 
+  analogWrite(ENA, 200);
 }
 
 void loop() {
-  // Rotation dans un sens
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, 200);  // Vitesse à 78% environ (200/255)
-  delay(2000);            // Tourne pendant 2 secondes
-
-  // Stoppe le moteur
-  analogWrite(ENA, 0);
-  delay(1000);
-
-  // Rotation dans l'autre sens
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  analogWrite(ENA, 200);
-  delay(2000);
-  
+ 
 }
