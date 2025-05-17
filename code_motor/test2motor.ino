8ae42ab6ae3104c455c57449af002a1e32aa163f
swyftos <98902001+swyftos@users.noreply.github.com>
2025-05-18 00:32:50 +0200

    Create test2motor.ino

---
+++ code_motor/test2motor.ino
@@ -0,0 +1,35 @@
+// Moteur A
+const int ENA = 2;  // Enable A (PWM)
+const int IN1 = 3;  // IN1
+const int IN2 = 4;  // IN2
+
+// Moteur B
+const int ENB = 7;  // Enable B (PWM)
+const int IN3 = 6;  // IN3
+const int IN4 = 5;  // IN4
+
+void setup() {
+  // Moteur A
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  // Moteur B
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Sens moteur A : avant
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 200);  // Vitesse moteur A
+
+  // Sens moteur B : avant (même que A)
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 200);  // Vitesse moteur B
+}
+
+void loop() {
+  // Rien ici : les deux moteurs tournent en continu
+}
